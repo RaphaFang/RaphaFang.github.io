2a1a93fd7ff728662ec6fa5b86d6b1ac03716dd6
RaphaFang <fangsihyu0211@gmail.com>
2024-04-08 16:39:37 +0800

    7?

--- 1_wehelp_weekly_challenge/w2_challenges_py&js/task1/task1.py
+++ 1_wehelp_weekly_challenge/w2_challenges_py&js/task1/task1.py
@@ -30,7 +30,11 @@ messages={
 stations={"Songshan":19,"Nanjing Sanmin":18,"Taipei Arena":17,"Nanjing Fuxing":16,"Songjiang Nanjing":15,"Zhongshan":14,"Beimen":13,"Ximen":12,"Xiaonanmen":11,"Chiang Kai-Shek Memorial Hall":10,"Guting":9,"Taipower Building":8,"Gongguan":7,"Wanlong":6,"Jingmei":5,"Dapinglin":4,"Xiaobitan":3.1,"Qizhang":3,"Xindian City Hall":2,"Xindian":1
 }
 
-find_and_print(messages, "Xindian City Hall") 
+find_and_print(messages, "Wanlong") # print Mary
+find_and_print(messages, "Songshan") # print Copper
+find_and_print(messages, "Qizhang") # print Leslie
+find_and_print(messages, "Ximen") # print Bob
+find_and_print(messages, "Xindian City Hall") # print Vivian
 print("====Task1 in py====")
 
 
@@ -38,9 +42,9 @@ print("====Task1 in py====")
 # 找到我車站的數值(dict, 站名:數值)、檢查messages中（編譯dict, 數值:人名）、
 
 # Ｏ解決：讀取json。實際上是dict了
-# Ｏ解決：處理捷運站序列，綠線
-#       key, para 要寫正確順序
+# Ｏ解決：處理捷運站序列，綠線，存粹打字處理
 # Ｏ解決：處理給定站名，回報順序
+#       key, para 要寫正確順序
 # Ｏ解決：處理messages找字串的功能。
 #       不是用 find 而是用 in
 #       dict comprehensive :
@@ -48,11 +52,13 @@ print("====Task1 in py====")
 #       https://www.udemy.com/course/100-days-of-code/learn/lecture/20763744#overview
 #       https://www.datacamp.com/tutorial/python-dictionary-comprehension
 # Ｏ解決：處理讀字典的功能，並且重新編寫
-#       透過map功能解決
+#       透過map、zip功能解決
 #       https://www.atatus.com/blog/python-converting-lsts-to-dictionaries/
-# 處理捷運斬「尋找」功能
+# Ｏ解決：處理捷運站距離比較功能
 #       絕對值：
 #       https://runoob.com/python/func-number-abs.html
+#       最大最小：
+#       透過先前wehelp code的紀錄
 # Ｏ解決：處理捷運綠線，注意有分岔出去的小碧潭站
 #       把小碧潭編列成3.1
 
