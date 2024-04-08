d906792b84997e0c18e6a434a55f2be2bd47437c
RaphaFang <fangsihyu0211@gmail.com>
2024-04-08 11:19:36 +0800

    first

---
+++ 1_wehelp_weekly_challenge/w2_challenges_py&js/task1/task1.py
@@ -0,0 +1,16 @@
+import json
+
+def find_and_print(messages, current_station):
+    pass
+messages={
+"Leslie":"I'm at home near Xiaobitan station.",
+"Bob":"I'm at Ximen MRT station.",
+"Mary":"I have a drink near Jingmei MRT station.",
+"Copper":"I just saw a concert at Taipei Arena.",
+"Vivian":"I'm at Xindian station waiting for you."
+}
+find_and_print(messages, "Wanlong") # print Mary
+find_and_print(messages, "Songshan") # print Copper
+find_and_print(messages, "Qizhang") # print Leslie
+find_and_print(messages, "Ximen") # print Bob
+find_and_print(messages, "Xindian City Hall") # print Vivian
