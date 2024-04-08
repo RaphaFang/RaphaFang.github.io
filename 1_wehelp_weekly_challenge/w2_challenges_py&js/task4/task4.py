bb9d225dea40ace83f6926c67ab964a904b47102
RaphaFang <fangsihyu0211@gmail.com>
2024-04-08 21:22:08 +0800

    puuuush

---
+++ 1_wehelp_weekly_challenge/w2_challenges_py&js/task4/task4.py
@@ -0,0 +1,13 @@
+def get_number(index):
+    tuple = divmod(index,3)
+    print(tuple[0]*7 + tuple[1]*4)
+
+
+# get_number(1) # print 4
+# get_number(5) # print 15
+# get_number(10) # print 25
+get_number(30) # print 70
+
+
+# Ｏ解決：了解py除法如何運作
+#       https://zoejoyuliao.medium.com/%E6%AF%94%E8%BC%83-python-%E7%9A%84-%E8%88%87-divmod-9f71786c0d33
