fecaf5b2a0cf95f40211e16024eed5b050c23887
RaphaFang <fangsihyu0211@gmail.com>
2024-04-09 15:37:10 +0800

    for a break

--- 1_wehelp_weekly_challenge/w2_challenges_py&js/task2/task2.py
+++ 1_wehelp_weekly_challenge/w2_challenges_py&js/task2/task2.py
@@ -1,14 +1,36 @@
-def get_price(element):
-    return element["price"]
+Jenny=[]
+Bob=[]
+Jenny=[]
+
+
+
+def get_price(ele):
+    return ele["price"]
 def get_rate(ele):
     return ele["rate"]
+def return_list(ele):
+    consultants.sort(key=ele)
+    return  [p['name'] for p in consultants]
 
-# def book(consultants, hour, duration, criteria):
-#     Jenny_dict=[]
-#     Bob_dict=[]
-#     Jenny_dict=[]
+def book(consultants, hour, duration, criteria):
+    available_time_dict = {}
+    price_order_list = return_list(get_price)
+    rate_order_list = return_list(get_rate)[::-1]
 
-    # price_order = 
+    if criteria=="price":
+        pass
+        for n in price_order_list:
+            print(type(n))
+            # if str(hour) not in n:
+            #     n.append(hour)
+            #     print(n)
+            #     return
+    else:
+        pass
+
+
+
+
     
         
 
@@ -17,15 +39,12 @@ consultants=[
 {"name":"Bob", "rate":3, "price":1200},
 {"name":"Jenny", "rate":3.8, "price":800}
 ]
-# price_order_dict ={p["name"]:p["price"] for p in consultants}
-# rate_order_dict ={r["name"]:r["rate"] for r in consultants}
-# price_order = [p for p in price_order_dict.sort(key=get_price)]
 
 
-consultants.sort(key=get_price)
-print(consultants)
 
-# book(consultants, 15, 1, "price") # Jenny
+# print(consultants)
+
+book(consultants, 15, 1, "price") # Jenny
 # book(consultants, 11, 2, "price") # Jenny
 # book(consultants, 10, 2, "price") # John
 # book(consultants, 20, 2, "rate") # John
@@ -36,6 +55,10 @@ print(consultants)
 
 
 # 條件順序：諮詢者名單順位、price/rate作為順序機制檢查空餘時間
-# Ｏ解決：先設定三個list，以人名為key
-# 將price/rate 各自建立list
-# 注意，如果直接print(consultants.sort(key=get_price))，會得出none
+# Ｘ錯誤：無法叫出這些list，且要考慮到func內部的資料在下次叫出時，可能不會存處
+    # Ｏ解決：先設定三個list，以人名為key
+    # Ｏ解決：將price/rate 各自建立list，透過dict.sort(key=['x'])，key是『函數』
+    #       https://www.freecodecamp.org/chinese/news/python-sort-how-to-sort-a-list-in-python/
+    #       注意，如果直接print(consultants.sort(key=get_price))，會得出none
+    # Ｏ解決：反轉列印出rate list。Slicing Operator
+    #       https://www.programiz.com/python-programming/methods/list/reverse
--- 1_wehelp_weekly_challenge/w2_challenges_py&js/task2/task2_scratch_sheet.py
+++ 1_wehelp_weekly_challenge/w2_challenges_py&js/task2/task2_scratch_sheet.py
@@ -13,3 +13,69 @@ def get_year(element):
 programming_languages.sort(key=get_year)
 
 print(programming_languages)
+
+# _______________________________________________________________________________
+Jenny=[]
+Bob=[]
+Jenny=[]
+
+
+
+def get_price(ele):
+    return ele["price"]
+def get_rate(ele):
+    return ele["rate"]
+def return_list(ele):
+    consultants.sort(key=ele)
+    return  [p['name'] for p in consultants]
+
+def book(consultants, hour, duration, criteria):
+    available_time_dict = {}
+    price_order_list = return_list(get_price)
+    rate_order_list = return_list(get_rate)[::-1]
+
+    if criteria=="price":
+        pass
+        for n in price_order_list:
+            print(type(n))
+            # if str(hour) not in n:
+            #     n.append(hour)
+            #     print(n)
+            #     return
+    else:
+        pass
+
+
+
+
+    
+        
+
+consultants=[
+{"name":"John", "rate":4.5, "price":1000},
+{"name":"Bob", "rate":3, "price":1200},
+{"name":"Jenny", "rate":3.8, "price":800}
+]
+
+
+
+# print(consultants)
+
+book(consultants, 15, 1, "price") # Jenny
+# book(consultants, 11, 2, "price") # Jenny
+# book(consultants, 10, 2, "price") # John
+# book(consultants, 20, 2, "rate") # John
+# book(consultants, 11, 1, "rate") # Bob
+# book(consultants, 11, 2, "rate") # No Service
+# book(consultants, 14, 3, "price") # John
+
+
+
+# 條件順序：諮詢者名單順位、price/rate作為順序機制檢查空餘時間
+# Ｘ錯誤：無法叫出這些list，且要考慮到func內部的資料在下次叫出時，可能不會存處
+    # Ｏ解決：先設定三個list，以人名為key
+    # Ｏ解決：將price/rate 各自建立list，透過dict.sort(key=['x'])，key是『函數』
+    #       https://www.freecodecamp.org/chinese/news/python-sort-how-to-sort-a-list-in-python/
+    #       注意，如果直接print(consultants.sort(key=get_price))，會得出none
+    # Ｏ解決：反轉列印出rate list。Slicing Operator
+    #       https://www.programiz.com/python-programming/methods/list/reverse
