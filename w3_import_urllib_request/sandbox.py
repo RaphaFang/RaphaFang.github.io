f06f6524604158ea4c5495df7077b4bc2558f97a
RaphaFang <fangsihyu0211@gmail.com>
2024-04-15 21:19:54 +0800

    complete the task1-1

--- w3_import_urllib_request/sandbox.py
+++ w3_import_urllib_request/sandbox.py
@@ -1,9 +1,47 @@
-station = ['文德', '中正紀念堂', '關渡', '西門', '松山', '關渡', '北投', '葫洲', '臺大醫院', '劍潭', '木柵', '忠孝新生', '市政府', '圓山', '芝山', '劍潭', '龍山寺', '公館', '新北投', '雙連', '士林', '士林', '新北投', '圓山', '大湖公園', '大直', '關渡', '劍潭', '石牌', '中山', '中山', '圓山', '忠義', '動物園', '松江南京', '雙連', '新北投', '中山', '國父紀念館', '士林', '動物園', '劍潭', '唭哩岸', '大安森林公園', '新北投', '象山', '龍山寺', '行天宮', '新北投', '中正紀念堂', '市政府', '動物園', '新北投', '關渡', '忠孝新生', '臺大醫院', '台北101／世貿', '龍山寺']
+import urllib.request as request
+import json
+import csv
+
+src1 = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+src2 = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2'
+
+with request.urlopen(src1) as response:
+    data1 = json.load(response)
+with request.urlopen(src2) as response:
+    data2 = json.load(response)
+
+# print(len(data1["data"]["results"]))
+station = [n['MRT'] for n in data2["data"]]
 for n in station:
     if station.count(n)!=1:
-        station.remove(n)
-print(station)
+        station.remove(n)  # >>>  處理station list串中重複出現
+name_to_mrt={k["MRT"]:k["address"] for k in data2["data"]}  # >>>  處理data2中站名是values的問題，並且組成[{站明：地址}}
 
-# list = [1, 2, 2, 3, 3, 3]
-# list.remove(3)
-# print(list)
+
+with open('RaphaFang.github.io/w3_import_urllib_request/spot.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    for n in range(len(data1["data"]["results"])):
+        SpotTitle = data1["data"]["results"][n]["stitle"]
+        for s in station:
+            if s+"站" in data1["data"]["results"][n]["info"]:
+                District = name_to_mrt[s][5:7]+"區"
+                break
+        Longitude = data1["data"]["results"][n]["longitude"]
+        Latitude = data1["data"]["results"][n]["latitude"]
+        if data1["data"]["results"][n]["filelist"].find(".jpg")==-1:
+            jpg = data1["data"]["results"][n]["filelist"].find(".JPG")
+        else:
+            jpg = data1["data"]["results"][n]["filelist"].find(".jpg" or ".JPG")
+        ImageURL = data1["data"]["results"][n]["filelist"][0:jpg+4]
+
+        writer.writerow([SpotTitle,District,Longitude,Latitude,ImageURL])
+
+
+# 解決ssl讀取阻擋：透過 Install Certificates.command
+#       https://support.chainstack.com/hc/en-us/articles/9117198436249-Common-SSL-Issues-on-Python-and-How-to-Fix-it
+# 解決地區名＼XX區＼經緯度＼第一張照片
+        
+
+with open('RaphaFang.github.io/w3_import_urllib_request/mrt.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
--- w3_import_urllib_request/task1.py
+++ w3_import_urllib_request/task1.py
@@ -1,41 +1,47 @@
 import urllib.request as request
 import json
+import csv
 
 src1 = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
 src2 = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2'
 
 with request.urlopen(src1) as response:
     data1 = json.load(response)
-
 with request.urlopen(src2) as response:
     data2 = json.load(response)
 
-# def 
-    
-
-
-# print(data1["data"]["results"][0]["stitle"])
+# print(len(data1["data"]["results"]))
 station = [n['MRT'] for n in data2["data"]]
-# station = [n for n in station if n]
 for n in station:
     if station.count(n)!=1:
-        station.remove(n)
-for n in station:
-    if n+"站" in data1["data"]["results"][0]["info"]:
-        data2["data"]["results"][0]["info"]
-        print(n)
-        break
-print(data1["data"]["results"][0]["info"])
-print(data1["data"]["results"][1]["info"])
-print(data1["data"]["results"][0]["longitude"])
-print(data1["data"]["results"][0]["latitude"])
-jpg = data1["data"]["results"][0]["filelist"].find(".jpg")
-print(data1["data"]["results"][0]["filelist"][0:jpg+4])
-print(station)
-print(1+1)
+        station.remove(n)  # >>>  處理station list串中重複出現
+name_to_mrt={k["MRT"]:k["address"] for k in data2["data"]}  # >>>  處理data2中站名是values的問題，並且組成[{站明：地址}}
 
-# ["latitude"]
+
+with open('RaphaFang.github.io/w3_import_urllib_request/spot.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    for n in range(len(data1["data"]["results"])):
+        SpotTitle = data1["data"]["results"][n]["stitle"]
+        for s in station:
+            if s+"站" in data1["data"]["results"][n]["info"]:
+                District = name_to_mrt[s][5:7]+"區"
+                break
+        Longitude = data1["data"]["results"][n]["longitude"]
+        Latitude = data1["data"]["results"][n]["latitude"]
+        if data1["data"]["results"][n]["filelist"].find(".jpg")==-1:
+            jpg = data1["data"]["results"][n]["filelist"].find(".JPG")
+        else:
+            jpg = data1["data"]["results"][n]["filelist"].find(".jpg" or ".JPG")
+        ImageURL = data1["data"]["results"][n]["filelist"][0:jpg+4]
+
+        writer.writerow([SpotTitle,District,Longitude,Latitude,ImageURL])
+
 
 # 解決ssl讀取阻擋：透過 Install Certificates.command
 #       https://support.chainstack.com/hc/en-us/articles/9117198436249-Common-SSL-Issues-on-Python-and-How-to-Fix-it
-# 解決地區明＼XX區＼經緯度＼第一張照片
+# 解決地區名＼XX區＼經緯度＼第一張照片
+        
+
+with open('RaphaFang.github.io/w3_import_urllib_request/mrt.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
