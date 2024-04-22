1955691b5e807da1c77fcc5e9b981e955fa8f322
RaphaFang <fangsihyu0211@gmail.com>
2024-04-22 18:37:26 +0800

    45

--- w4_FastAPI/project_directory/main.py
+++ w4_FastAPI/project_directory/main.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Mount the static directory for CSS and JavaScript files
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Load templates from the templates directory
+# import html file
 templates = Jinja2Templates(directory="templates")
+@app.get("/", response_class=HTMLResponse) # By declaring response_class=HTMLResponse the docs UI will be able to know that the response will be HTML.
+async def display_html(request: Request):
+    return templates.TemplateResponse(name = "api.html", request = request)
+# https://fastapi.tiangolo.com/advanced/templates/
 
-# Define routes
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/login/")
+# mount css file
+app.mount("/static_css", StaticFiles(directory="static_css"), name="static_css")
+# https://fastapi.tiangolo.com/tutorial/static-files/
+# Mount the static directory for CSS and JavaScript files
+
+# mount css file
+@app.get("/")
 async def login(username: str = Form(...), password: str = Form(...), remember: bool = Form(...)):
     # Your login logic here
     return {"username": username, "password": password, "remember": remember}
