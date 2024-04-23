7cfd95970ebabc5eb3a8c9f5ec9050a9f763545a
RaphaFang <fangsihyu0211@gmail.com>
2024-04-24 00:59:03 +0800

    we

--- w4_FastAPI/project_directory/main.py
+++ w4_FastAPI/project_directory/main.py
@@ -13,27 +13,27 @@ app.mount("/static", StaticFiles(directory="static"), name="style")
 # 第一个 "/static" 是 URL 路径前缀。directory="static" 是服务器上的文件夹路径。name="static" 是这个挂载点的内部名称，用于应用程序内部引用。
 # https://fastapi.tiangolo.com/tutorial/static-files/
 
-def html_generator(text):
-    html_content = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <title>login page</title>
-        <link rel="stylesheet" href="static/successful.css">
-        <!-- <script src="/static/script.js"></script> -->
-      </head>
-      <body>
-        <div class="container">
-        <div class="login-header">
-            <p>失敗頁面</p>
-        </div>
-        <p class = "text">{text}</p></br></p>
-        </div>
-      </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+# def html_generator(text):
+    # html_content = f"""
+    # <!DOCTYPE html>
+    # <html lang="en">
+    #   <head>
+    #     <meta charset="UTF-8" />
+    #     <title>login page</title>
+    #     <link rel="stylesheet" href="static/successful.css">
+    #     <!-- <script src="/static/script.js"></script> -->
+    #   </head>
+    #   <body>
+    #     <div class="container">
+    #     <div class="login-header">
+    #         <p>失敗頁面</p>
+    #     </div>
+    #     <p class = "text">{text}</p></br></p>
+    #     </div>
+    #   </body>
+    # </html>
+    # """
+    # return HTMLResponse(content=html_content, status_code=200)
 
 # import html file
 templates = Jinja2Templates(directory="templates")
@@ -45,15 +45,17 @@ async def display_html(request: Request):
 
 @app.get("/member", response_class=HTMLResponse)
 async def redirect_successful_html(request: Request):
-    return templates.TemplateResponse(name = "successful.html", request = request)
+    return templates.TemplateResponse(name = "successful.html", context={"request" : request},request = request)
+    # return templates.TemplateResponse(name = "successful.html", request = request)
+
 
 @app.get("/error", response_class=HTMLResponse)
 async def show_error_page(request: Request,  message: str = ""):
-    return templates.TemplateResponse(name = "error.html", request= request, message= message)
+    return templates.TemplateResponse(name = "error.html", context={"request": request, "message": message},request = request)
 
 # https://fastapi.tiangolo.com/zh-hant/tutorial/request-forms-and-files/?h=form
 @app.post("/signin")
-async def login(username: Optional[str] = Form(None) , password:Optional[str] = Form(None), accept: bool = Form(...)):
+async def login(username: Optional[str] = Form(None) , password:Optional[str] = Form(None), accept: bool = Form()):
     user_info =  {"username": username, "password": password, "accept": accept}
     if user_info["username"] == "test" and user_info["password"] == "test":
         return RedirectResponse(url='/member', status_code=303)
