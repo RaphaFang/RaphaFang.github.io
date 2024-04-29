8124fc006c92aa7f5f20211bce0ea5f91442be2a
RaphaFang <fangsihyu0211@gmail.com>
2024-04-29 16:43:45 +0800

    5

--- w5_MySQL/root@localhost.session.sql
+++ w5_MySQL/root@localhost.session.sql
@@ -8,22 +8,55 @@
 --     time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Signup Time'
 -- );
 
--- syntex Error
--- CREATE TABLE users (
---     id bigint PRIMARY KEY AUTO INCREMENT 'Unique ID',
---     name VARCHAR(255) NOT NULL 'Name',
---     username VARCHAR(255)NOT NULL 'Username',
---     password VARCHAR(255) NOT NULL 'Password',
---     follower_count int unsigned not null default to 0 'Follower Count',
---     time datetime not null default to current time 'Signup Time'
--- );
 
 
-
--- SHOW DATABASES website;
-
-DESCRIBE member;
+-- 重新命名table
 -- RENAME TABLE users TO member;
 
-INSERT INTO users (name, username, password, follower_count, time)
-VALUES ('John Doe', 'johndoe', 'password123', 0, NOW());
+-- 顯示 Table Structure
+-- DESCRIBE member;
+
+-- 顯示 table data, Querying Data from the Table
+-- SELECT * FROM member;
+
+-- 3.1. 增加table data 資訊
+-- INSERT INTO member (name, username, password)
+-- VALUES ('test', 'test', 'test');
+-- INSERT INTO member (name, username, password)
+-- VALUES ('arbitrary_1', 'ab1', '1234');
+-- INSERT INTO member (name, username, password)
+-- VALUES ('arbitrary_2', 'ab2', '1234');
+-- INSERT INTO member (name, username, password)
+-- VALUES ('arbitrary_3', 'ab3', '1234');
+-- INSERT INTO member (name, username, password)
+-- VALUES ('arbitrary_4', 'ab4', '1234');
+
+-- 3.2. 全選資料
+-- SELECT * FROM member;
+
+-- 3.3. 選擇全部data，且降序排列
+-- SELECT * FROM member ORDER BY time DESC;
+
+-- 3.4. 降序排列，且選擇2~4(排除1), 這邊換行可以不用加上";"
+-- SELECT * FROM member ORDER BY time DESC
+-- LIMIT 3 OFFSET 1;
+    -- 如果基於id 選擇用：WHERE id = 1;
+
+-- 3.5. 透過username 篩選
+-- SELECT * FROM member
+-- WHERE username = 'test';
+
+-- 3.6. 選擇name 中包含 'es'
+-- SELECT * FROM member
+-- WHERE name LIKE '%es%';
+
+-- 3.7. username and password equal to test
+-- SELECT * FROM member
+-- WHERE username = "test" AND password = "test"
+
+-- 3.8. UPDATE data in name column to test2 where username equals to test.
+-- UPDATE member
+-- SET name="test2"
+-- WHERE name = "test";
+
+SELECT * FROM member
