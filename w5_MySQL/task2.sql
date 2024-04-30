53955a45031c3b98d93dd329383549e3c6f774d8
RaphaFang <fangsihyu0211@gmail.com>
2024-04-30 15:06:05 +0800

    1

---
+++ w5_MySQL/task2.sql
@@ -0,0 +1,10 @@
+USE website;
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT 'Unique ID',
+    name VARCHAR(255) NOT NULL COMMENT 'Name',
+    username VARCHAR(255) NOT NULL COMMENT 'Username',
+    password VARCHAR(255) NOT NULL COMMENT 'Password',
+    follower_count INT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Follower Count',
+    time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Signup Time'
+);
+
