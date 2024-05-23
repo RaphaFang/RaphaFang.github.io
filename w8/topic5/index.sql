425580bf1a0b96dc20b7903519b2be2f33939f14
RaphaFang <fangsihyu0211@gmail.com>
2024-05-23 17:01:51 +0800

    d

--- w8/topic5/index.sql
+++ w8/topic5/index.sql
@@ -5,11 +5,22 @@
 
 -- 5.2
 -- normal
--- SELECT * FROM member WHERE username='test' and password='test';
+SELECT * FROM member WHERE username='test' and password='test';
 
 -- index speed up
--- CREATE INDEX idx_username_password ON member (username, password);
+CREATE INDEX idx_username_password ON member (username, password);
 
 
 -- 5.3
 EXPLAIN SELECT * FROM member WHERE username='test' and password='test';
+
+-- 5.4
+
+-- 5.5
+CREATE INDEX like_testing_index ON member (username);
+-- 'LIKE abc' is equivalent to '= abc'
+
+-- prefix matches
+EXPLAIN SELECT * FROM member WHERE username LIKE 'ab%';
+-- suffix matches
+EXPLAIN SELECT * FROM member WHERE username LIKE '%4';
