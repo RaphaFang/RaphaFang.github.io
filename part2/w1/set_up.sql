2d6d457621a3038d973387496d7674da730f5c85
RaphaFang <fangsihyu0211@gmail.com>
2024-05-27 16:30:38 +0800

    pp

---
+++ part2/w1/set_up.sql
@@ -0,0 +1,15 @@
+SHOW DATABASES;
+USE basic_db;
+
+CREATE TABLE processed_data (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    category TEXT,
+    description TEXT,
+    address TEXT,
+    transport TEXT,
+    mrt TEXT,
+    lat REAL,
+    lng REAL,
+    images TEXT
+);
