c4131ee0f6c266a7268ab2d1cc064569e3d109db
RaphaFang <fangsihyu0211@gmail.com>
2024-06-17 13:35:48 +0800

    pp

--- weekly_discussion/part2_w4/main.js
+++ weekly_discussion/part2_w4/main.js
@@ -54,3 +54,65 @@ class MyClass {
 }
 const myInstance = new MyClass();
 myInstance.myMethod(); // "Class method is called"
+
+// --------------------------------------------------------
+// 寫入
+function fetchAndStoreImage(url, storageKey) {
+  fetch(url)
+    .then((response) => response.blob()) // 將資料轉換為 Blob （Binary Large Object）
+    .then((blob) => {
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        sessionStorage.setItem(storageKey, reader.result); // 將 Base64 字符串存儲到 SessionStorage
+        console.log("Image stored in SessionStorage");
+      };
+      reader.readAsDataURL(blob); // 讀取為 Base64 字符串，更好的成現在hyml
+    });
+}
+fetchAndStoreImage("url", "storageKey");
+
+// --------------------------------------------------------
+// 讀取
+document.addEventListener("DOMContentLoaded", () => {
+  loadImageFromSessionStorage("storageKey", "replacedImgElementId");
+});
+
+function loadImageFromSessionStorage(storageKey, replacedImgElementId) {
+  const dataURL = sessionStorage.getItem(storageKey);
+  if (dataURL) {
+    const imgElement = document.getElementById(replacedImgElementId);
+    imgElement.src = dataURL;
+  }
+}
+
+// --------------------------------------------------------
+// function fetchAndStoreImages(apiUrl, storageKey) {
+//   fetch(apiUrl)
+//     .then((response) => response.json()) // 假設 API 返回一個包含多個圖片 URL 的 JSON 數組
+//     .then((imageUrls) => {
+//       // 創建一個 Promise 列表，每個 Promise 處理一個圖片 URL
+//       const fetchPromises = imageUrls.map((url) =>
+//         fetch(url)
+//           .then((response) => response.blob())
+//           .then((blob) => {
+//             return new Promise((resolve, reject) => {
+//               const reader = new FileReader();
+//               reader.onloadend = () => {
+//                 resolve(reader.result);
+//               };
+//               reader.onerror = reject;
+//               reader.readAsDataURL(blob);
+//             });
+//           })
+//       );
+//       return Promise.all(fetchPromises);
+//     })
+//     .then((base64Images) => {
+//       // 將處理後的 Base64 圖片數組存儲在 SessionStorage 中
+//       sessionStorage.setItem(storageKey, JSON.stringify(base64Images));
+//       console.log("Images stored in SessionStorage");
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching images:", error);
+//     });
+// }
