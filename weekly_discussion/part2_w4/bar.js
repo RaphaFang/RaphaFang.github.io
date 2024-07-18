fdd53659e1e82a58340ba038cadd119b0b6c3788
RaphaFang <fangsihyu0211@gmail.com>
2024-07-18 21:07:25 +0800

    save the project

---
+++ weekly_discussion/part2_w4/bar.js
@@ -0,0 +1,10 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  backToMainPage();
+});
+function backToMainPage() {
+  const titleElement = document.getElementById("title");
+  titleElement.addEventListener("click", function () {
+    window.location.href = "/";
+  });
+}
